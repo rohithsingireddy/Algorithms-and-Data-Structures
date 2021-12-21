01413b02c95321ba0b5bcf44f7d48f46f01c9dd5
Rohith <rohithdevprofile@gmail.com>
2021-12-21 19:51:18 +0530

    Small changes

--- CPP/Data_Structures/Suffix_Tree.h
+++ CPP/Data_Structures/Suffix_Tree.h
@@ -14,31 +14,24 @@ private:
     {
         int start, end, length;
         std::unordered_map<char, Node *> edges;
-        Node *suffix_link;
+        Node *suffix_link, *parent;
 
-        Node(int start = -1, int end = -1, int length = 0)
+        explicit Node(Node *parent, int start = -1, int end = -1)
         {
             this->start = start;
             this->end = end;
-            this->length = length;
+            this->length = end - start;
             this->suffix_link = nullptr;
-            this->edges['\0'] = this;
+            this->parent = parent;
         }
 
-        bool is_in_edge(const std::string &s, char active_edge, int active_length, int index)
+        void update_indices(int start, int end)
         {
-            if (active_length == 0)
-            {
-                return this->edges.find(s[index]) != this->edges.end();
-            }
-            else
-            {
-                Node *cur = this->edges[active_edge];
-                int char_index = cur->start + active_length;
-                return s[char_index] == s[index];
-            }
+            this->start = start;
+            this->end = end;
+            this->length = end - start;
         }
-    } * root;
+    } * root, *NIL;
 
     struct Active
     {
@@ -55,101 +48,155 @@ private:
 
     } active;
 
-    void insert_node(int index, Node *prev = nullptr)
+    void insert_node(Node *current, int start, int end)
     {
-        Node *current = this->active.node;
-        if (current->is_in_edge(this->text, this->active.edge, this->active.length, index))
+        Node *new_node = new Node(current, start, end);
+
+        current->edges.insert({this->text[start], new_node});
+    }
+
+    Node *split_node(Node *cur, Node *prev_split = nullptr)
+    {
+        Node *new_node = new Node(cur->parent, cur->start, cur->start + this->active.length);
+        // printf("%d ----- %d\n", cur->start, cur->end);
+
+        cur->parent->edges[this->text[cur->start]] = new_node;
+
+        cur->update_indices(new_node->end, cur->end);
+        new_node->edges.insert({this->text[cur->start], cur});
+        cur->parent = new_node;
+
+        new_node->suffix_link = cur->suffix_link;
+        cur->suffix_link = nullptr;
+
+        if (prev_split != nullptr)
         {
-            if (this->active.length + 1 < current->edges[this->active.edge]->length)
+            prev_split->suffix_link = new_node;
+        }
+
+        return new_node;
+    }
+
+    bool is_in_active_edge(int index)
+    {
+        if (this->active.node->edges.find(this->active.edge) != this->active.node->edges.end())
+        {
+            Node *cur = this->active.node->edges[this->active.edge];
+            if (this->active.edge == '\0')
             {
-                this->active.set(current, this->active.edge, this->active.length + 1);
+                return cur->edges.find(this->text[index]) != cur->edges.end();
+            }
+            int char_index = cur->start + this->active.length;
+            return this->text[char_index] == this->text[index];
+        }
+        return false;
+    }
+
+    void insert(int index)
+    {
+
+        if (this->is_in_active_edge(index))
+        {
+            Node *cur = this->active.node->edges[this->active.edge];
+            if (this->active.length + 1 >= cur->length)
+            {
+                this->active.set(
+                    cur,
+                    this->text[index],
+                    1);
             }
             else
             {
-                this->active.set(current->edges[this->text[index]], '\0', 1);
+                this->active.set(
+                    this->active.node,
+                    this->active.edge,
+                    this->active.length + 1);
             }
             this->remainder++;
         }
         else
         {
-            if (this->active.length != 0)
+            // printf("\\\\\\\\ %d %c \\\\\\\\\\\n", index, this->active.edge);
+            Node *last_split_node = nullptr;
+            int prev_index = index - this->remainder + 1;
+            while (this->remainder > 0)
             {
-                this->split_node(current->edges[this->active.edge], index, prev);
-            }
-            else
-            {
-                Node *new_node = new Node(index, this->size - 1, this->size - index);
-                current->edges.insert({this->text[index], new_node});
+                Node *temp;
+                prev_index++;
+                if (this->active.node->edges.find(this->active.edge) != this->active.node->edges.end())
+                {
+                    temp = this->active.node->edges[this->active.edge];
+                }
+                else
+                {
+                    temp = this->active.node;
+                }
+                if (this->active.length > 0)
+                {
+                    temp = this->split_node(temp, last_split_node);
+                    if (this->active.node == this->root)
+                    {
+                        this->active.set(
+                            this->active.node,
+                            (prev_index < this->size) ? this->text[prev_index] : '\0',
+                            this->active.length - 1);
+                    }
+                    else
+                    {
+                        this->active.set(
+                            (this->active.node->suffix_link == nullptr)
+                                ? this->root
+                                : this->active.node->suffix_link,
+                            this->active.edge,
+                            this->active.length);
+                    }
+                    last_split_node = temp;
+                }
+                else
+                {
+                        this->active.set(
+                            this->NIL,
+                            '\0',
+                            0);
+                }
+                // printf("******** %c ********\n", this->active.edge);
+                this->insert_node(temp, index, this->size);
                 this->remainder--;
+                if (this->is_in_active_edge(index))
+                {
+                    break;
+                }
             }
         }
-    }
-
-    void split_node(Node *cur, int s_index, Node *prev_split)
-    {
-        if (prev_split != nullptr)
-        {
-            prev_split->suffix_link = cur;
-        }
-        
-        cur->end = cur->start + this->active.length - 1;
-        cur->length = this->active.length;
-
-        Node *new_node_1 = new Node(
-            cur->end + 1, 
-            this->size - 1, 
-            this->size - cur->end - 1);
-        
-        Node *new_node_2 = new Node(
-            s_index, 
-            this->size - 1, 
-            this->size - s_index);
-
-        cur->edges.insert({this->text[cur->end + 1], new_node_1});
-        cur->edges.insert({this->text[s_index], new_node_2});
-        this->remainder--;
-
-        if (this->active.node == this->root)
-        {
-            this->active.set(
-                this->root,
-                this->text[s_index - this->remainder + 1],
-                this->active.length - 1);
-        }
-        else
-        {
-            this->active.set(
-                (cur->suffix_link == nullptr) ? this->root : cur->suffix_link,
-                this->active.edge,
-                this->active.length);
-        }
-        this->insert_node(s_index, cur);
     }
 
     void create_suffix_tree()
     {
         for (int i = 0; i < this->size; i++)
         {
-            if (this->remainder <= 0)
-            {
-                this->remainder = 1;
-            }
-            this->insert_node(i);
+            this->remainder = (this->remainder <= 0) ? 1 : this->remainder;
+            this->insert(i);
         }
     }
 
 public:
     explicit Suffix_Tree(const std::string s, char delimiter = '$')
     {
-        this->root = new Node();
-
-        this->active = Active();
-        this->active.set(this->root, '\0', 0);
 
         this->text = s + delimiter;
         this->size = this->text.size();
         this->remainder = 0;
 
+        this->NIL = new Node(nullptr, -1, -1);
+        this->root = new Node(this->NIL);
+
+        for (int i = 0; i < 256; i++)
+        {
+            NIL->edges.insert({(char)i, this->root});
+        }
+
+        this->active = Active();
+        this->active.set(this->NIL, '\0', 0);
         this->create_suffix_tree();
     }
 
@@ -160,15 +207,14 @@ public:
 
     void dfs(Node *current, std::string s)
     {
-        if (current == nullptr) 
+        if (current == nullptr)
             return;
-        printf("%d %d\n", current->start, current->end);
         std::string append = "";
         bool flag = false;
 
         if (current != this->root)
         {
-            append = this->text.substr(current->start, current->length);
+            append = this->text.substr(current->start, current->end - current->start);
         }
 
         for (int i = 1; i < 256; i++)
@@ -180,6 +226,7 @@ public:
             }
         }
 
+        // printf("%d %d\n", current->start, current->end);
         if (!flag)
         {
             for (char c : s + append)
