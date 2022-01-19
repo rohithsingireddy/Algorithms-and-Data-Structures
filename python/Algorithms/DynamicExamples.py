bd726dc07e46932db3ee43cfeaa9157e132e7fdc
Rohith <rohithdevprofile@gmail.com>
2022-01-19 14:30:07 +0530

    Changed some wrong logic

--- python/Algorithms/DynamicExamples.py
+++ python/Algorithms/DynamicExamples.py
@@ -1,6 +1,6 @@
 from math import inf
 
-def max_price_for_rod(price_for_length:list):
+def max_price_for_rod(price_for_length:list) -> tuple:
     '''
         Rod cutting problem. Finds the maximum price possible given a list of prices for different lengths
         Returns the maximum price and the possible lengths to cut the rod into
@@ -10,45 +10,44 @@ def max_price_for_rod(price_for_length:list):
     dp_array = [i for i in price_for_length]
     dp_array.insert(0, 0)
 
-    for cut in range(max_length+1):
-        for length in range(cut, max_length+1):
+    for length in range(1, max_length+1):
+        for cut in range(length):
             if dp_array[length] < dp_array[cut] + dp_array[length - cut]:
                 dp_array[length] = dp_array[cut] + dp_array[length - cut]
                 cut_off[length] = cut
     
-    result = [dp_array[max_length]]
+    result = []
 
     cut = max_length
     while cut > 0:
         result.append(cut_off[cut])
         cut -= cut_off[cut]
     
-    return result
+    return dp_array[max_length], result
 
 
 def common_subsequence( string_1:str, string_2:str)-> str:
     '''
-        Finds the common subsequence between two given strings
+        Finds the largest common subsequence between two given strings
         Returns the string with the sequence of characters
-        Should correct for input abracadabraemalesmagic abracadabrayfemalemagic
     '''
     string_mat = []
-    for i in range(len(string_1)):
-        string_mat.append([0 for _ in range(len(string_2))])
+    for i in range(len(string_1) + 1):
+        string_mat.append([0 for _ in range(len(string_2) + 1)])
 
-    for i in range(len(string_1)):
-        for j in range(len(string_2)):
-            if string_1[i] == string_2[j]:
+    for i in range(1, len(string_1) + 1):
+        for j in range(1, len(string_2) + 1):
+            if string_1[i - 1] == string_2[j - 1]:
                 string_mat[i][j] = 1 + string_mat[i-1][j-1]
             else:
                 string_mat[i][j] = max(string_mat[i-1][j], string_mat[i][j-1])
 
-    i, j = len(string_1)-1, len(string_2)-1
-    common_string = ''
+    i, j = len(string_1), len(string_2)
+    common_string = []
 
-    while i >= 0  and j >= 0 :
-        if string_1[i] == string_2[j]:
-            common_string += string_1[i]
+    while i > 0  and j > 0 :
+        if string_1[i - 1] == string_2[j - 1]:
+            common_string.append(string_1[i - 1])
             i -= 1
             j -= 1
         elif string_mat[i-1][j] > string_mat[i][j-1]:
@@ -56,7 +55,7 @@ def common_subsequence( string_1:str, string_2:str)-> str:
         else:
             j -= 1
 
-    common_string = ''.join([x for x in reversed(common_string)])
+    common_string.reverse()
     return common_string
 
 
